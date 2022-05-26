0fb512cb30228ad993bb3d73d9c7620b27fe81bb
Amit pariente <106227905+amitpariente@users.noreply.github.com>
2022-05-26 20:40:40 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3,22 +3,28 @@
 var gBoard
 const MINE = '💣'
 const RED_FLAG = '🚩'
-var isflag = false
+var LIVE = '💖'
+var gLives = 3  
 var gStartTime
 var gIntervalId
 var gLevel = {
     SIZE: 4,
     MINES: 2
 }
+var gIsFirstClick 
 
 
 function init() {
-
+    gIsFirstClick=false
+    var elButn = document.querySelector('.fill')
+    elButn.innerText = '😃'
+    gLives=3
     gBoard = buildBoard()
     renderBoard(gBoard)
-    createMines(gBoard)
     console.table(gBoard);
-    updateSetMinesNegsCount()
+    updateLives()
+   
+   
 
 
 }
@@ -57,29 +63,44 @@ function setMinesNegsCount(cellI, cellJ, board) {
 //       isMarked: true }
 
 function cellClicked(elCell, cellI, cellJ) {
-    // startTimer()
     console.log(elCell);
+    if (!gIsFirstClick){
+        startTimer()
+        gIsFirstClick=true
+        createMines(gBoard)
+        updateSetMinesNegsCount()
+    }
     var cell = gBoard[cellI][cellJ]
+    if (cell.isMarked === true) return
     if (cell.isMine) {
-        gameOver()
-        return
+        renderCell(cellI,cellJ,MINE)
+        var mineElement = new Audio('../sound/mines.mp4');
+        cell.isShown=true
+        mineElement.play();
+        gLives--
+        updateLives()
+        elCell.classList='cell'
+        if (gLives === 0) {
+            gameOver()
+            return
+        }
     } else if (!cell.isShown) {
         cell.isShown = true
         elCell.classList = 'clicked'
         if (cell.minesAroundCount === 0) {
-            var neighbors= findAllNeighbors(cellI,cellJ,gBoard)
-            for(var i=0;i<neighbors.length;i++){
-                var neighbor= neighbors[i]
-                neighbor.classList =  'clicked'
-                neighbor.isShown=true
-                console.log(neighbor);
+            var neighbors = findAllNeighbors(cellI, cellJ, gBoard)
+            for (var i = 0; i < neighbors.length; i++) {
+                var neighbor = neighbors[i]
+                var elNighCell = document.querySelector(`[data-i="${neighbor.i}"][data-j="${neighbor.j}"]`);
+                cellClicked(elNighCell,neighbor.i,neighbor.j)
             }
-            
             return
         }
         renderCell(cellI, cellJ, cell.minesAroundCount)
     }
+    checkWin()
 }
+
 
 
 
@@ -87,35 +108,44 @@ function cellClicked(elCell, cellI, cellJ) {
 function gameOver() {
     var elButn = document.querySelector('.fill')
     elButn.innerText = '😫'
+    clearInterval(gIntervalId)
 
 }
 
 function restart() {
-    var elButn = document.querySelector('.fill')
-    elButn.innerText = '😃'
+    clearInterval(gIntervalId)
+    var elTimer = document.querySelector('.timer')
+    elTimer.innerText=''
     init()
 }
 
 function winner() {
     var elButn = document.querySelector('.fill')
     elButn.innerText = '😎'
-
+    clearInterval(gIntervalId)
 }
 
 function easy() {
     gLevel.SIZE = 4
     gLevel.MINES = 2
+    clearInterval(gIntervalId)
+    var elTimer = document.querySelector('.timer')
+    elTimer.innerText=''
     init()
-
 }
 
 function medium() {
+    clearInterval(gIntervalId)
+    var elTimer = document.querySelector('.timer')
+    elTimer.innerText=''
     gLevel.SIZE = 8
     gLevel.MINES = 12
     init()
 }
 
 function hard() {
+    var elTimer = document.querySelector('.timer')
+    elTimer.innerText=''
     gLevel.SIZE = 12
     gLevel.MINES = 30
     init()
@@ -130,9 +160,10 @@ function updateTime() {
     var now = Date.now()
     var diff = now - gStartTime
     var secPast = diff / 1000
+    var secPastFix = setTimer(secPast)
     var elTimer = document.querySelector('.timer')
     console.log(elTimer)
-    elTimer.innerText = secPast
+    elTimer.innerText = secPastFix
 
 
 }
@@ -147,15 +178,16 @@ function updateSetMinesNegsCount() {
     }
 }
 
-function findAllNeighbors(cellI,cellJ,board) {
-    var neighbors=[]
+function findAllNeighbors(cellI, cellJ, board) {
+    var neighbors = []
     for (var i = cellI - 1; i <= cellI + 1; i++) {
         if (i < 0 || i >= board.length) continue;
         for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+
             if (i === cellI && j === cellJ) continue;
             if (j < 0 || j >= board[i].length) continue;
             var cell = board[i][j]
-            neighbors.push(cell)
+            neighbors.push({i:i,j:j})
         }
     }
     return neighbors
--- js/create.js
+++ js/create.js
@@ -1,34 +1,44 @@
 'use strict'
 
-function createFlag(elCell,ev) {
+function createFlag(elCell, i, j, ev) {
     ev.preventDefault()
     console.log(elCell);
+    var isflag = gBoard[i][j].isMarked
     if (!isflag) {
         elCell.innerHTML = RED_FLAG
-        isflag=true
-        elCell.isMarked=true
+        gBoard[i][j].isMarked = true
 
 
-    }else if (isflag){
-        elCell.innerHTML=''
-        isflag=false
+    } else if (isflag) {
+        elCell.innerHTML = ''
+        gBoard[i][j].isMarked = false
     }
     return false
 }
 
-function createMine(board){
-   var emptyCells=getEmptyCells(board)
-    var index = getRandomInt(0,emptyCells.length)
+function createMine(board) {
+    var emptyCells = getEmptyCells(board)
+    var index = getRandomInt(0, emptyCells.length)
     console.log(emptyCells[index]);
     // model
     board[emptyCells[index].i][emptyCells[index].j].isMine = true
+
     // dom
-    renderCell(emptyCells[index].i,emptyCells[index].j, MINE)
+    // renderCell(emptyCells[index].i,emptyCells[index].j, MINE)
 }
 
-function createMines(board){
-    for (var i=0;i<gLevel.MINES;i++){
+function createMines(board) {
+    for (var i = 0; i < gLevel.MINES; i++) {
         createMine(board)
     }
+}
+
+function updateLives() {
+    var strLives=''
+    for (var i = 0; i < gLives; i++) {
+        strLives += LIVE
+    }
+    var elLives = document.querySelector('.lives')
+    elLives.innerText = strLives
 }
 
